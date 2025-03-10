e5acb23dad0b51d6e8fd55a4a4c8dca058f40ada
*vivek <vrvivekofficial@gmail.com>
2025-03-10 14:18:25 +0530

    config moddlewares and create server and test

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { config } from "./config/app.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Hello",
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log("server is running", config.PORT);
+});
